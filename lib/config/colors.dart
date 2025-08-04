6c93f88e6800eea566752f84db184af0baa3e9d8
nikhilesh-7119 <nikhilesh7119off@gmail.com>
2025-08-04 18:07:17 +0530

    first commit

---
+++ lib/config/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const dBackgroundColor=Color(0xff191B28);
+const dPrimaryColor=Color(0xff10057FF);
+const dContainerColor=Color(0xff343748);
+const donBackgroundColor=Color(0xffFFFFFF);
+const donContainerColor=Color(0xff9E9E9E);
---
+++ lib/config/themes.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:sampark_web/Config/Colors.dart';
+import 'package:sampark_web/config/colors.dart';
+
+var lightTheme = ThemeData();
+var darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  useMaterial3: true,
+  appBarTheme: AppBarTheme(
+    backgroundColor: dContainerColor,
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    fillColor: dBackgroundColor,
+    filled: true,
+    border: UnderlineInputBorder(
+      borderSide: BorderSide.none,
+      borderRadius: BorderRadius.circular(10),
+    ),
+  ),
+  colorScheme: const ColorScheme.dark(
+    primary: dPrimaryColor,
+    onPrimary: donBackgroundColor,
+    background: dBackgroundColor,
+    onBackground: donBackgroundColor,
+    primaryContainer: dContainerColor,
+    onPrimaryContainer: donContainerColor,
+  ),
+  textTheme: const TextTheme(
+    headlineLarge: TextStyle(
+      fontSize: 32,
+      color: dPrimaryColor,
+      fontFamily: "Poppins",
+      fontWeight: FontWeight.w800,
+    ),
+    headlineMedium: TextStyle(
+      fontSize: 30,
+      color: donBackgroundColor,
+      fontFamily: "Poppins",
+      fontWeight: FontWeight.w600,
+    ),
+    headlineSmall: TextStyle(
+      fontSize: 20,
+      color: donBackgroundColor,
+      fontFamily: "Poppins",
+      fontWeight: FontWeight.w600,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 18,
+      color: donBackgroundColor,
+      fontFamily: "Poppins",
+      fontWeight: FontWeight.w500,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 15,
+      color: donBackgroundColor,
+      fontFamily: "Poppins",
+      fontWeight: FontWeight.w500,
+    ),
+    labelLarge: TextStyle(
+      fontSize: 15,
+      color: donContainerColor,
+      fontFamily: "Poppins",
+      fontWeight: FontWeight.w400,
+    ),
+    labelMedium: TextStyle(
+      fontSize: 12,
+      color: donContainerColor,
+      fontFamily: "Poppins",
+      fontWeight: FontWeight.w400,
+    ),
+    labelSmall: TextStyle(
+      fontSize: 10,
+      color: donContainerColor,
+      fontFamily: "Poppins",
+      fontWeight: FontWeight.w300,
+    ),
+  ),
+);
